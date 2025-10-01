52eb25e4634c97b48eb193fc2d101f53690655c9
rahuldarekar222 <139219289+rahuldarekar222@users.noreply.github.com>
2025-10-01 18:43:20 +0530

    Update SampleApp.java

--- SampleApp.java
+++ SampleApp.java
@@ -20,6 +20,7 @@ import java.util.*;
 
 // const hostname = '0.0.0.0';
 // const port = 3000;
+
 public class Main {
     // Placeholder for sequelize, User, Password
     // In Java we have no direct "sequelize" or "User" model as defined.
